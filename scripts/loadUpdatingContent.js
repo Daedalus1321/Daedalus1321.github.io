02c947d1f5d2a5e27ac7e7766bb28ad3b8b02c03
Asher Hausam <ahausam1321@gmail.com>
2025-02-13 00:04:30 -0600

    fix js

--- scripts/loadUpdatingContent.js
+++ scripts/loadUpdatingContent.js
@@ -1,5 +1,6 @@
 function update(){        
-        document.getElementById("top_bar").innerHTML = '<a href="https://asherhausam.com">'+
+    let topBar = document.getElementById("top_bar");   
+    topBar.innerHTML = '<a href="https://asherhausam.com">'+
     '<span class="mso fit" >home</span></a>'+
     ' | ' +
     '<a href="/blog/"><span class="mso fit">menu_book</span>blog</a>' +
@@ -8,7 +9,8 @@ function update(){
     ' | ' +
     '<a href="/sitemap/"><span class="mso fit">menu</span>more</a>';
         
-        document.getElementById("socials").innerHTML = '<a href="https://www.linkedin.com/in/asher-hausam-208a2021a/"><span class="mso fit">work</span>LinkedIn</a>'+
+    let socials=document.getElementById("socials");
+        socials.innerHTML = '<a href="https://www.linkedin.com/in/asher-hausam-208a2021a/"><span class="mso fit">work</span>LinkedIn</a>'+
     ' | '+
     '<a href="https://www.instagram.com/asherhausam/"><span class="mso fit">photo_camera</span>Instagram</a>'+
     ' | '+
