460b7f11df6c936f4d175ad5308c3adebaac59c3
Asher Hausam <ahausam1321@gmail.com>
2025-02-13 00:02:17 -0600

    updated links and header

--- scripts/loadUpdatingContent.js
+++ scripts/loadUpdatingContent.js
@@ -1,7 +1,16 @@
-function update(){
-    //"C:\Users\ahaus\git\Daedalus1321.github.io\scripts\loadUpdatingContent.js"    
-    
-        let element = document.getElementById("links");
+function update(){        
+        document.getElementById("top_bar").innerHTML = '<a href="https://asherhausam.com">'+
+    '<span class="mso fit" >home</span></a>'+
+    ' | ' +
+    '<a href="/blog/"><span class="mso fit">menu_book</span>blog</a>' +
+    ' | ' +
+    '<a href="/projects/"><span class="mso fit">code</span>projects</a>' +
+    ' | ' +
+    '<a href="/sitemap/"><span class="mso fit">menu</span>more</a>';
         
-        element.innerHTML = '<a href="https://asherhausam.com"><span class="mso fit" >home</span></a> | <a href="/blog/"><span class="mso fit">menu_book</span>blog</a> | <a href="/rpg/"><span class="mso fit" style="color:red">strategy</span>rpg</a>';
+        document.getElementById("socials").innerHTML = '<a href="https://www.linkedin.com/in/asher-hausam-208a2021a/"><span class="mso fit">work</span>LinkedIn</a>'+
+    ' | '+
+    '<a href="https://www.instagram.com/asherhausam/"><span class="mso fit">photo_camera</span>Instagram</a>'+
+    ' | '+
+    '<a href="https://github.com/Daedalus1321"><span class="mso fit">code</span>Github</a>'
     }
