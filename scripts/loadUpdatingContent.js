bab0c3d37b91019048a7551bc3b49b471cb95221
Asher Hausam <ahausam1321@gmail.com>
2025-02-12 23:23:39 -0600

    JS updating headers and footers

---
+++ scripts/loadUpdatingContent.js
@@ -0,0 +1,7 @@
+function update(){
+    //"C:\Users\ahaus\git\Daedalus1321.github.io\scripts\loadUpdatingContent.js"    
+    
+        let element = document.getElementById("links");
+        
+        element.innerHTML = '<a href="https://asherhausam.com"><span class="mso fit" >home</span></a> | <a href="/blog/"><span class="mso fit">menu_book</span>blog</a> | <a href="/rpg/"><span class="mso fit" style="color:red">strategy</span>rpg</a>';
+    }
