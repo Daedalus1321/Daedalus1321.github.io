b8864879fe48bcffc577afdc9a18627ba92e1df7
Asher Hausam <ahausam1321@gmail.com>
2025-08-22 00:40:46 -0500

    updated "site updated"

--- scripts/loadUpdatingContent.js
+++ scripts/loadUpdatingContent.js
@@ -18,8 +18,8 @@ function updateLinks(){
     '<a href="https://github.com/Daedalus1321"><span class="mso fit">code</span>Github</a>'
     
     let lastDate=document.getElementById("update");
-            lastDate.innerHTML = '<p>This site last updated : 2025/08/08</p>'
+            lastDate.innerHTML = '<p>This site last updated : 2025/08/22</p>'
             lastDate.style.fontSize = "18px";
-            lastDate.style.color = "rgb(45,45,45)";
+            lastDate.style.color = "rgb(70,70,70)";
             
     }
